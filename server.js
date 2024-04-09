dd0e4376e24319a767c6f43eeef1a0928bc15330
devsamuelsilva <devsamuelsilva@gmail.com>
2024-04-09 19:41:22 -0300

    criacao do endpoints com POST GET e GETpoID

--- server.js
+++ server.js
@@ -46,28 +46,51 @@ fastify.get('/produtos', async (request, reply) => {
   }
 });
 
-// Utilizando o metodo de post para adicionar um novo produto ao banco
-// fastify.post('/produtos', async (request, reply) => {
-//   const { nome, categoria, preco } = request.body;
+fastify.get('/produto/:id', async (request, reply) => {
+  const id = request.params.id;
+  //DOLAR1($1) e usado apra receber o dados do paramentro posto no RESULT
+  const query = 'SELECT * FROM produtos WHERE id=$1'
+  try {
+    const result = await config.query(query,[id]);
+    reply.send(result.rows);
+  } catch (err) {
+    console.log(err);
+    reply.send(500);
+  }
+});
 
-//   const query = 'INSERT INTO produtos (nome, categoria, preco) VALUES ("Produto Teste", "TESTE", $9999,99)';
 
-//   try {
-//     await config.query(query, [nome, categoria, preco]);
-//     reply.code(201).send({ message: 'Produto criado com sucesso!' });
-//   } catch (err) {
-//     console.error(err);
-//     reply.code(400).send({ message: 'Erro ao inserir produto' });
-//   }
-// });
+//Utilizando o metodo de post para adicionar um novo produto ao banco
+fastify.post('/produtos', async (request, reply) => {
+  const { nome, categoria, preco, desconto } = request.body;
+
+  //const query = 'INSERT INTO produtos (nome, categoria, preco, desconto) VALUES ($1, $2, $3, $4)';
+  
+  //retornando o objeto inserido. (RETURNING no final sempre trará o que foi pedido da query, para retornar todos as colunas RETURNING *)
+  //DOLAR1($1) dentro do VALUE e usado apra receber o dados do paramentro posto no RESULT
+  const query = 'INSERT INTO produtos (nome, categoria, preco, desconto) VALUES ($1, $2, $3, $4) RETURNING *';
+
+  try {
+   const result = await config.query(query, [nome, categoria, preco, desconto]);
+    reply.code(201).send({ 
+      message: 'Produto criado com sucesso!', 
+      data: result.rows[0]
+    });
+  } catch (err) {
+    console.error(err);
+    reply.code(400).send({ message: 'Erro ao inserir produto' });
+  }
+});
 
 fastify.delete('/produtos/:id', async (req, res) => {
-  await deleteProduto(req.params.id);
-  res.status(204);
+  const id = req.params.id;
+  const query = `DELETE FROM produtos WHERE id = ${id}`;
+  console.log(`[QUERY]: ${query}`);
+  const result = await config.query(query);
+  // res.json()
+  res.status(204).send(`Produto de id: ${id} deletado com sucesso !!`);
+  return result;
 })
-
-
-
 
 
 // Run the server!
