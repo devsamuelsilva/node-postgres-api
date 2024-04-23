9e0e38b4d38f9fa4cd2a52d3e69fab72ff4a171c
devsamuelsilva <devsamuelsilva@gmail.com>
2024-04-23 19:36:47 -0300

    Configurando o banco do Render

--- DB/config.js
+++ DB/config.js
@@ -2,9 +2,10 @@ import pg from 'pg';
 const { Pool } = pg;
 
 export const config = new Pool({
-  host: 'localhost',
+  host: 'dpg-cok2ign79t8c73a4g440-a.oregon-postgres.render.com',
   port: 5432,
-  database: 'dc_22',
-  user: 'postgres',
-  password: '123456',
+  database: 'dcf22',
+  user: 'samuel',
+  password: 'p1nRFjxppzXtQgig8y4YDjtGK9qhh7m4',
+  ssl: true,
 });
